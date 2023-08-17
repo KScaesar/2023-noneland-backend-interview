de4e9d9a45888f9f99967a9009b080619544b61c
caesar <x246libra@hotmail.com>
2023-08-17 22:46:57 +0800

    feat: err package

---
+++ internal/pkg/errors/summary_error.go
@@ -0,0 +1,44 @@
+package errors
+
+import "net/http"
+
+var (
+	ErrInvalidToken = NewCustomError(
+		"invalid token",
+		700,
+		http.StatusUnauthorized,
+	)
+	ErrTokenExpired = NewCustomError(
+		"token expired",
+		710,
+		http.StatusUnauthorized,
+	)
+
+	ErrInvalidParams = NewCustomError(
+		"invalid params",
+		800,
+		http.StatusBadRequest,
+	)
+	ErrNotFound = NewCustomError(
+		"not found",
+		810,
+		http.StatusNotFound,
+	)
+	ErrKeyDuplicated = NewCustomError(
+		"key or name is duplicated",
+		820,
+		http.StatusConflict,
+	)
+
+	ErrSystem = NewCustomError(
+		"system fail",
+		900,
+		http.StatusInternalServerError,
+	)
+
+	ErrUnknown3rdParty = NewCustomError(
+		"unknown third party",
+		-1,
+		http.StatusInternalServerError,
+	)
+)
