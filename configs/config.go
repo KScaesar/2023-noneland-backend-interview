33f923635f8d4df7a454a5d74ab03ee145dad37f
caesar <x246libra@hotmail.com>
2023-08-17 22:34:35 +0800

    refactor: read config path

--- configs/config.go
+++ configs/config.go
@@ -1,11 +1,16 @@
 package configs
 
 import (
+	"bytes"
+	"embed"
 	"fmt"
 	"os"
 
 	"github.com/spf13/viper"
 )
+
+//go:embed *
+var directory embed.FS
 
 type Config struct {
 	Port string
@@ -13,18 +18,22 @@ type Config struct {
 }
 
 // NewConfigFromFilename 為了在寫測試的時候, 可以方便抽換設定值
+// 原本的寫法寫死相對路徑, 當進行測試時 呼叫此函數的時候會失敗
 func NewConfigFromFilename(filename string) *Config {
 	err := os.Setenv("TZ", "UTC")
 	if err != nil {
 		panic(fmt.Errorf("fatal error configs file: set time zone to utc: %w", err))
 	}
 
+	buf, err := directory.ReadFile(filename + ".env")
+	if err != nil {
+		panic(fmt.Errorf("fatal error configs file: read config file: %w", err))
+	}
+
 	viper.AutomaticEnv()
 	viper.SetConfigName(filename)
 	viper.SetConfigType("env")
-	viper.AddConfigPath("./configs")
-
-	err = viper.ReadInConfig()
+	err = viper.ReadConfig(bytes.NewReader(buf))
 	if err != nil {
 		panic(fmt.Sprintf("讀取設定檔出現錯誤，原因為：%v", err))
 	}
